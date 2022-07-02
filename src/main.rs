d96b2dc3a596a5eb5344d590b73be61e5cf5b24b
Zach Krzyzanowski <zkrzyz@gmail.com>
2022-07-02 18:00:59 -0400

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use jsonwebtoken::{Algorithm, DecodingKey, TokenData, Validation};
 use serde::{Deserialize, Serialize};
-use serde_json::Value;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Claims {
@@ -14,14 +13,12 @@ struct Claims {
 
 fn main() {
     let mut validation = Validation::new(Algorithm::RS256);
-    validation.algorithms = vec![];
     validation.insecure_disable_signature_validation();
 
     let token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJub25lIn0.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTY1Njc5MjY1MSwiZXhwIjoxNjU2Nzk2MjUxfQ.";
-    let decoding_key = DecodingKey::from_secret("fake_key".as_ref());
 
     let decoded_token: Result<TokenData<Claims>, jsonwebtoken::errors::Error> =
-        jsonwebtoken::decode::<Claims>(token, &decoding_key, &validation);
+        jsonwebtoken::decode::<Claims>(token, &DecodingKey::from_secret(&[]), &validation);
 
     match decoded_token {
         Ok(token) => println!("{:?}", token.header),
